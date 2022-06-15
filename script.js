9dfb4fbf69e8ccc9319bc6feed9d107f444f26c1
Olly Seber <olly_s@hotmail.co.uk>
2022-06-15 11:20:57 -0700

    Implement basic calculator functions
    The Calculator class operates based on the values stored in three
    variables:
     - firstOperand
     - operator
     - secondOperand
    
    When a numerical digit is pressed, the correct operand can be updated
    (i.e. appended with the new digit) depending on whether "operator" has
    already been stored. If no "operator" exists, the firstOperand is
    updated. If "operator" is defined, then the secondOperand is updated.
    
    The Calculator class will attempt to calculate the result of the
    equation described by the three variables firstOperand, operator,
    secondOperand under two conditions:
     - the equals button is pressed (provided a full operation is described
       by the three variables)
     - an operator button is pressed AND a full operation is ALREADY
       described by the three variables
    
    In the second case, this allows the "chaining" of multiple operations
    without the need for the equals button to be pressed. The result of the
    previous operation becomes the firstOperand of the next operation.

--- script.js
+++ script.js
@@ -20,25 +20,68 @@ const Calculator = {
     firstOperand: null,
     secondOperand: null,
     operator: null,
+   
     updateDisplay(result) {
         this.display.textContent = result;
         this.displayValue = result;
     },
+   
     operate() {
         let result;
         switch (this.operator) {
             case "add":
-                result = add(this.firstOperand, this.secondOperand);
+                result = add(Number(this.firstOperand), Number(this.secondOperand));
                 break;
             case "subtract":
-                result = subtract(this.firstOperand, this.secondOperand);
+                result = subtract(Number(this.firstOperand), Number(this.secondOperand));
                 break;
             case "multiply":
-                result = multiply(this.firstOperand, this.secondOperand);
+                result = multiply(Number(this.firstOperand), Number(this.secondOperand));
                 break;
             case "divide":
-                result = divide(this.firstOperand, this.secondOperand);
+                result = divide(Number(this.firstOperand), Number(this.secondOperand));
                 break;
         }
-    }
+        this.updateDisplay(result);
+        this.firstOperand = result;
+        this.operator = null;
+        this.secondOperand = null;
+    },
+
+    operatorPressed(operator) {
+        if (this.operator && this.secondOperand) {
+            this.operate();
+        }
+        if (this.firstOperand) {
+            this.operator = operator;
+        }
+    },
+
+    appendDigit(currentValue, digit) {
+        if (currentValue) {
+            return currentValue + digit;
+        } else {
+            return digit;
+        }
+    },
+
+    digitPressed(digit) {
+        if (!this.operator) {
+           this.firstOperand = this.appendDigit(this.firstOperand, digit);
+        } else {
+            this.secondOperand = this.appendDigit(this.secondOperand, digit);
+        }
+    },
 };
+
+function setup() {
+    document.querySelectorAll(".operator").forEach( button => {
+        button.addEventListener("click", (e) => Calculator.operatorPressed(e.target.id));
+    });
+
+    document.getElementById("equals").addEventListener("click", () => Calculator.operate());
+
+    document.querySelectorAll(".digit").forEach( digit => digit.addEventListener("click", (e) => Calculator.digitPressed(e.target.textContent)));
+};
+
+setup();
